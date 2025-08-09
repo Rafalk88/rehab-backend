3c58eac98b73773544a21b97cb3cc9644dcbadc0
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-08-09 19:34:36 +0200

    feat(jwt): add warn when no secret

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,6 +3,9 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export const generateToken = (payload: object) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('API Secret not defined. Unable to generate JWT.');
+  }
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 };
 
