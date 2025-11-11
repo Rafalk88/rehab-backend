821f7b3af5fa46e2bc6b7203cae7f7d86661820b
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-11-11 04:38:17 +0100

    feat(seed): update seed

--- src/seed/main.ts
+++ src/seed/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { SeedModule } from './seed.module.js';
 import { SeedService } from './seed.service.js';
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from '../prisma/prisma.service.js';
 import { computeHmac, aesGcmEncrypt, maskString } from '../lib/encryption.util.js';
 import { hashPassword } from '../lib/password.util.js';
 
-const CURRENT_KEY_VERSION = 1;
+const SYSTEM_USER_ID = '00000000-0000-0000-0000-000000000001';
 
 @Injectable()
 export class SeedService {
@@ -13,17 +13,27 @@ export class SeedService {
     const firstName = await this.prisma.givenName.upsert({
       where: { firstName: 'test' },
       update: {},
-      create: { firstName: 'test' },
+      create: { firstName: 'test', createdBy: SYSTEM_USER_ID, updatedBy: SYSTEM_USER_ID },
     });
 
     const surname = await this.prisma.surname.upsert({
       where: { surname: 'user' },
       update: {},
-      create: { surname: 'user' },
+      create: { surname: 'user', createdBy: SYSTEM_USER_ID, updatedBy: SYSTEM_USER_ID },
     });
+
+    const sexes = ['mężczyzna', 'kobieta', 'nienadane'];
+    for (const sex of sexes) {
+      await this.prisma.sex.upsert({
+        where: { sex },
+        update: {},
+        create: { sex, createdBy: SYSTEM_USER_ID, updatedBy: SYSTEM_USER_ID },
+      });
+    }
 
     const login = 'tuser';
     const email = `${login}@vitala.com`;
+    const CURRENT_KEY_VERSION = 1;
 
     const loginHmac = computeHmac(login, CURRENT_KEY_VERSION);
     const loginEncrypted = aesGcmEncrypt(login, CURRENT_KEY_VERSION);
@@ -31,9 +41,12 @@ export class SeedService {
 
     const emailHmac = computeHmac(email, CURRENT_KEY_VERSION);
     const emailEncrypted = aesGcmEncrypt(email, CURRENT_KEY_VERSION);
-    const emailMasked = maskString(email);
+    const emailMasked = `${loginMasked}@vitala.com`;
 
     const passwordHash = await hashPassword('Test2025!!!!');
+
+    const male = await this.prisma.sex.findUnique({ where: { sex: 'mężczyzna' } });
+    if (!male) throw new Error('Sex "mężczyzna" not found');
 
     await this.prisma.user.upsert({
       where: { loginHmac },
@@ -51,6 +64,9 @@ export class SeedService {
         isActive: true,
         firstNameId: firstName.id,
         surnameId: surname.id,
+        sexId: male.id,
+        createdBy: SYSTEM_USER_ID,
+        updatedBy: SYSTEM_USER_ID,
       },
     });
   }
