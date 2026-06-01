77fdcbcc5241d2db939ec10225e767d142507d2f
Rafalk88 <rafal.kochanecki@gmail.com>
2026-06-01 19:48:29 +0200

    feat(organizational-units): implement method

--- src/modules/organizational-units/organizational-units.service.ts
+++ src/modules/organizational-units/organizational-units.service.ts
@@ -1,4 +1,14 @@
+import { PrismaService } from '#prisma/prisma.service.js';
 import { Injectable } from '@nestjs/common';
 
+/**
+ * This service provides method to retrieve organizational units from the database using Prisma ORM.
+ */
 @Injectable()
-export class OrganizationalUnitsService {}
+export class OrganizationalUnitsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getOrganizationalUnits() {
+    return this.prisma.organizationalUnit.findMany();
+  }
+}
