d6e9d4d835bb33a60d2ea02f65c7d51b6ed42b72
Rafalk88 <rafal.kochanecki@gmail.com>
2026-06-01 20:12:22 +0200

    feat(organizational-units): add GET /organizational-units endpoint

--- src/modules/organizational-units/organizational-units.controller.ts
+++ src/modules/organizational-units/organizational-units.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { OrganizationalUnitsService } from './organizational-units.service.js';
+import { AuthorizationGuard } from '#common/guards/authorization.guard.js';
+import { JwtAuthGuard } from '#modules/auth/v1/guards/jwt-auth.guard.js';
+import { Controller, UseGuards, Get } from '@nestjs/common';
 
 @Controller('organizational-units')
-export class OrganizationalUnitsController {}
+export class OrganizationalUnitsController {
+  constructor(private readonly organizationalUnitsService: OrganizationalUnitsService) {}
+
+  @UseGuards(JwtAuthGuard, AuthorizationGuard)
+  @Get()
+  async getOrganizationalUnits() {
+    return this.organizationalUnitsService.getOrganizationalUnits();
+  }
+}
