2f241588625c2307b2f8296184ec91f231dc0090
Rafalk88 <rafal.kochanecki@gmail.com>
2026-06-29 22:16:34 +0200

    test(visits): add create method test

--- src/modules/visits/visits.service.spec.ts
+++ src/modules/visits/visits.service.spec.ts
@@ -1,13 +1,28 @@
 import { VisitsService } from './visits.service.js';
-import { RequestContextService } from '#context/request-context.service.js';
+import {
+  RequestContextService,
+  type RequestContextData,
+} from '#context/request-context.service.js';
 import { PrismaService } from '#prisma/prisma.service.js';
 import { createPrismaMock, type MockPrisma } from '#tests/helpers/prisma-mock.js';
+import type { Prisma } from '#generated/prisma/client.js';
 import { jest } from '@jest/globals';
 import { Test, TestingModule } from '@nestjs/testing';
+
+type PatientType = Prisma.PatientGetPayload<{
+  include: { firstName: true; surname: true };
+}>;
+
+type OrganizationalUnitType = Prisma.OrganizationalUnitGetPayload<{}>;
+
+type VisitWithPatient = Prisma.VisitGetPayload<{
+  include: { patient: { include: { firstName: true; surname: true } } };
+}>;
 
 describe('VisitsService', () => {
   let service: VisitsService;
   let prisma: MockPrisma;
+  let requestContext: RequestContextData;
 
   beforeEach(async () => {
     prisma = createPrismaMock();
@@ -30,9 +45,51 @@ describe('VisitsService', () => {
     }).compile();
 
     service = module.get<VisitsService>(VisitsService);
+    requestContext = module.get(RequestContextService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a visit successfully', async () => {
+      prisma.patient.findUnique.mockResolvedValueOnce({
+        id: 'patient-1',
+        firstName: { firstName: 'Jan' },
+        surname: { surname: 'Kowalski ' },
+      } as PatientType);
+
+      prisma.organizationalUnit.findUnique.mockResolvedValueOnce({
+        id: 'orgUnit-1',
+      } as OrganizationalUnitType);
+
+      prisma.visit.create.mockResolvedValueOnce({
+        id: 'visit-1',
+        patientId: 'patient-1',
+        patient: {
+          id: 'patient-1',
+          firstName: {
+            firstName: 'Jan',
+          },
+          surname: {
+            surname: 'Kowalski',
+          },
+        },
+      } as VisitWithPatient);
+
+      const result = await service.create({
+        patientId: 'patient-1',
+        organizationalUnitId: 'orgUnit-1',
+        plannedDate: '2026-06-26',
+        status: 'PLANNED',
+      });
+
+      expect(result).toMatchObject(
+        expect.objectContaining({
+          patient: expect.objectContaining({
+            id: 'patient-1',
+            firstName: expect.objectContaining({ firstName: 'Jan' }),
+            surname: expect.objectContaining({ surname: 'Kowalski' }),
+          }),
+        }),
+      );
+    });
   });
 });
